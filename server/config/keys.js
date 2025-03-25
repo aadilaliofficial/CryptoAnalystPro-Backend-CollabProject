da13add6a32ca63947686458facc05e906e2f3f0
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-03-25 14:48:03 +0530

    Delete server directory

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-import "dotenv";
-
-export default {
-    coingecko: process.env.COIN_GECKO_API_KEY,
-    etherscanApiKey: process.env.ETHERSCAN_API_KEY,
-    geminiApiKey: process.env.GEMINI_API_KEY,
-    cryptoNewsApiKey: process.env.CRYPTO_NEWS_API_KEY,
-};
--- server/routes/crypto.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getCryptoPrices } from "../controllers/crypto.controller.js";
-import isAuthenticated from "../middleware/isAuthenticate.js";
-
-const router = express.Router();
-
-router.route("/price").get(isAuthenticated, getCryptoPrices);
-
-export default router;
